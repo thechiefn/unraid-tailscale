31d93e8d14228f5df3002661b5f7108adce2cf5a
Joe Crandall <jjcrandall@gmail.com>
2022-09-10 10:45:02 -0600

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -11,6 +11,11 @@ if [ ! -c /dev/net/tun ]; then
     mknod /dev/net/tun c 10 200
 fi
 
+# Enable forwarding (VPN Gateway)
+echo 'net.ipv4.ip_forward = 1' | sudo tee -a /etc/sysctl.conf
+echo 'net.ipv6.conf.all.forwarding = 1' | sudo tee -a /etc/sysctl.conf
+sudo sysctl -p /etc/sysctl.conf
+
 # Start the daemon
 /app/tailscaled --state=/state/tailscaled.state --statedir=/state/ &
 
@@ -25,6 +30,7 @@ if [ "${ENABLE_DOWNLOADS}" = "TRUE" ]; then
     echo "Enabling downloads"
     /app/tailscale file get -wait=true -conflict=rename -loop=true /downloads &
 fi
+
 
 
 # Do nothing until the end of time
